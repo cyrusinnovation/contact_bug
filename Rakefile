85b85d77a42f5f903236779a34202703939ca200
Paul Infield-Harm <paul@infield-harm.org>
2012-05-23 20:24:12 -0400

    Added a few more files

--- Rakefile
+++ Rakefile
@@ -4,6 +4,6 @@ require 'motion/project'
 Motion::Project::App.setup do |app|
   # Use `rake config' to see complete project settings.
   app.name = "Contact Crash"
-  app.frameworks += ['AddressBook', 'AddressBookUI']
+  app.frameworks += ['AddressBookUI']
 
 end
--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -2,49 +2,33 @@ class AppDelegate
   
   def application(application, didFinishLaunchingWithOptions:launchOptions)
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
-    @window.rootViewController = SampleViewController.alloc.init
-    @window.rootViewController.wantsFullScreenLayout = true
+    @window.rootViewController = ABPeoplePickerNavigationController.alloc.init
+    @window.rootViewController.peoplePickerDelegate = self
     @window.makeKeyAndVisible
     true
   end
- 
-end
 
-class SampleViewController < UIViewController
-  
-  def viewDidLoad
-    @action = UIButton.buttonWithType(UIButtonTypeContactAdd)
-    @action.setTitle('Pick a Contact')
-    @action.addTarget(self, action:'showPicker', forControlEvents:UIControlEventTouchUpInside)
-    @action.frame = [[20,0], [100,100]]
-    view.addSubview(@action)
+  def peoplePickerNavigationController(peoplePicker, shouldContinueAfterSelectingPerson:person,
+                                  property:property, identifier:identifier)
+
+      puts
+      puts ">>>>>>>> Getting the record id"
+      record_id = ABRecordGetRecordID(person)
+      puts ">>>>>>>> the record id is: #{record_id}"
+
+      puts ">>>>>>>> About to get first name"
+      first_name = ABRecordCopyValue(person, KABPersonFirstNameProperty)
+      puts ">>>>>>>> Here's the first name: #{first_name}"
+      return true
   end
-  
-  def showPicker
-    picker = ABPeoplePickerNavigationController.alloc.init
-    picker.peoplePickerDelegate = self
-    self.presentModalViewController(picker,animated:true)
-  end  
-  
+
   def peoplePickerNavigationControllerDidCancel(peoplePicker)
-    self.dismissModalViewControllerAnimated(true)
-    @current_cell = nil
+    # left blank
   end
-
 
   def peoplePickerNavigationController(peoplePicker, shouldContinueAfterSelectingPerson:person)
     return true
   end
 
-  def peoplePickerNavigationController(peoplePicker, shouldContinueAfterSelectingPerson:person,
-                                  property:property, identifier:identifier)
-      # record_id = ABRecordGetRecordID(person)
-      # puts "the record id is: #{record_id}"
-      puts "About to copy value"
-      first_name = ABRecordCopyValue(person, KABPersonFirstNameProperty)
-      puts "First name: #{first_name}"
-      return true
-  end
-  
-  
+ 
 end
